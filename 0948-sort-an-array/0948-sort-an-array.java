b7d2e8b2a3714361ffa97a0455f9a338da6c0c83
Lakshman <128733062+Lakshman2504@users.noreply.github.com>
2025-08-08 08:02:00 +0530

    Time: 67 ms (25.41%), Space: 55.5 MB (85.57%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -20,16 +20,20 @@ class Solution {
         List<Integer> temp = new ArrayList<>();
         while(right <= high && left <= mid){
             if(nums[left] <= nums[right]){
-                temp.add(nums[left++]);
+                temp.add(nums[left]);
+                left++;
             }else{
-                temp.add(nums[right++]);
+                temp.add(nums[right]);
+                right++;
             }
         }
         while(left <= mid){
-            temp.add(nums[left++]);
+            temp.add(nums[left]);
+            left++;
         }
         while(right <= high){
-            temp.add(nums[right++]);
+            temp.add(nums[right]);
+            right++;
         }
         for(int i = low;i <= high;i++){
             nums[i] = temp.get(i - low);
