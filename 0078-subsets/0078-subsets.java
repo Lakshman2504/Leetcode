f0bef470a857b7e012b040ca331af44e83f6809d
Lakshman <128733062+Lakshman2504@users.noreply.github.com>
2025-08-08 08:10:53 +0530

    Time: 1 ms (49.38%), Space: 42.8 MB (46.03%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -4,15 +4,15 @@ class Solution {
         recursion(0,nums,new ArrayList<Integer>(),res);
         return res;
     }
-    public static void recursion(int index,int[] nums,List<Integer> temp,List<List<Integer>> res){
-        if(index == nums.length){
+    public static void recursion(int index,int[] arr,List<Integer> temp,List<List<Integer>> res){
+        if(index == arr.length){
             res.add(new ArrayList<>(temp));
             return;
         }
-        temp.add(nums[index]);
-        recursion(index + 1,nums,temp,res);
+        temp.add(arr[index]);
+        recursion(index + 1,arr,temp,res);
 
         temp.remove(temp.size() - 1);
-        recursion(index + 1,nums,temp,res);
+        recursion(index + 1,arr,temp,res);
     }
 }
