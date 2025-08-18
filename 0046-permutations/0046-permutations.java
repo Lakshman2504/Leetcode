eb1ff7bbdab8e337c50c64a0b6b3033c24a14337
Lakshman <128733062+Lakshman2504@users.noreply.github.com>
2025-08-18 08:17:45 +0530

    Time: 1 ms (92.55%), Space: 44.7 MB (29.5%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,23 +1,27 @@
 class Solution {
-    public void recursion(int index,int[] arr,List<List<Integer>> res,List<Integer> temp,boolean[] used){
+    public void recursion(int index,int[] arr,List<List<Integer>> res){
         if(index == arr.length){
+            List<Integer> temp = new ArrayList<>();
+            for(int i = 0;i<arr.length;i++){
+                temp.add(arr[i]);
+            }
             res.add(new ArrayList<>(temp));
             return;
         }
-        for(int i = 0;i<arr.length;i++){
-            if(!used[i]){
-                used[i] = true;
-                temp.add(arr[i]);
-                recursion(index + 1,arr,res,temp,used);
-                temp.remove(temp.size() - 1);
-                used[i] = false;
-            }
+        for(int i = index;i<arr.length;i++){
+            swap(arr,i,index);
+            recursion(index + 1,arr,res);
+            swap(arr,i,index);
         }
+    }
+    public void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        boolean[] used = new boolean[nums.length];
-        recursion(0,nums,res,new ArrayList<>(),used);
+        recursion(0,nums,res);
         return res;
     }
 }
